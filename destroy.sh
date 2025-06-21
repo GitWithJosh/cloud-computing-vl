294d214ce45557e4b15c095bdd4e392ad9c4c9e2
GitWithJosh <meyer.joshua@gmx.de>
2025-06-21 18:00:13 +0200

    removed unnecessary deploy and destroy scripts

--- destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🧹 Use the new version manager instead:"
-echo "./version-manager.sh cleanup"
-echo ""
-echo "Or for direct cleanup:"
-source openrc.sh
-terraform destroy -auto-approve
